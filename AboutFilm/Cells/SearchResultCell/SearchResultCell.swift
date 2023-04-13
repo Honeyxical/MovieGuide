1104246279d54d4d81157a807df10be51048dfe9
Ilya Benikov <ilya.benikov@gmail.com>
2023-04-13 22:37:25 +0400

    Created search result cell

---
+++ AboutFilm/Cells/SearchResultCell/SearchResultCell.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    
+    var film: SearchFilmResult? = nil
+
+    @IBOutlet weak private var posterImageView: UIImageView!
+    
+    @IBOutlet weak private var filmNameLabel: UILabel!
+    
+    @IBOutlet weak private var secondNameYearLabel: UILabel!
+    
+    @IBOutlet weak private var countryGenresLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureFilmNameLabel()
+        configureSecondNameYearLabel()
+        configureCountryGenresLabel()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func configureCell(){
+        guard let film = film else {
+            return
+        }
+        posterImageView.image = UIImage(data: film.posterData ?? Data())
+        filmNameLabel.text = film.name!
+        secondNameYearLabel.text = film.alternativeName! == "" ? String(film.year!) : "\(film.alternativeName!), \(String(film.year!))"
+        countryGenresLabel.text = arrayToString(array: film.countries!) + " • " + arrayToString(array: film.genres!)
+    }
+    
+    private func configureFilmNameLabel(){
+        filmNameLabel.font = UIFont.systemFont(ofSize: 15, weight: UIFont.Weight.bold)
+        filmNameLabel.numberOfLines = 1
+    }
+    
+    private func configureSecondNameYearLabel(){
+        secondNameYearLabel.numberOfLines = 1
+        secondNameYearLabel.font = UIFont.systemFont(ofSize: 14)
+        secondNameYearLabel.textColor = .lightGray
+    }
+    
+    private func configureCountryGenresLabel(){
+        countryGenresLabel.textColor = UIColor.gray
+        countryGenresLabel.numberOfLines = 2
+        countryGenresLabel.font = UIFont.systemFont(ofSize: 14)
+    }
+}
