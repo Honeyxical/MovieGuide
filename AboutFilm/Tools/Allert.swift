bf71c87d3b08304eab5bf96c43992e856fb80f40
Ilya Benikov <ilya.benikov@gmail.com>
2023-04-13 22:46:11 +0400

    Move convertMTH method into converter

--- AboutFilm/Tools/Allert.swift
+++ AboutFilm/Tools/Allert.swift
@@ -6,15 +6,3 @@ func getAllert(message: String) -> UIAlertController{
     allert.addAction(UIAlertAction(title: "OK", style: .default))
     return allert
 }
-
-func convertMTH(min: Int) -> String{
-    var min = min
-    var hours = 0
-    
-    repeat {
-        hours += 1
-        min = min - 60
-    } while (min > 60);
-    
-    return String(hours) + "h" + String(min) + "m"
-}
