5d5e5cfedaf24ac4965f7a6120d06b7b5a623a95
Ilya Benikov <ilya.benikov@gmail.com>
2023-04-13 22:47:10 +0400

    Created file with help methods

---
+++ AboutFilm/Tools/Converter.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+func convertMTH(min: Int) -> String{
+    var min = min
+    var hours = 0
+    
+    repeat {
+        hours += 1
+        min = min - 60
+    } while (min > 60);
+    
+    return String(hours) + "h" + String(min) + "m"
+}
+
+func arrayToString(array: [String?]) -> String{
+    if array.isEmpty{
+        return ""
+    }
+    var result = ""
+    for i in 0...array.count - 1{
+        result.append(array[i]! + " ")
+    }
+    return result
+}
+
+func genresToString(array: [Generes?]) -> String{
+    var result = ""
+    for i in 0...array.count - 1{
+        result.append(array[i]!.name! + " ")
+    }
+    return result
+}
