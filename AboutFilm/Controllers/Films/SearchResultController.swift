b508d657b308f5dbf71c53045e045bd74099a847
Ilya Benikov <ilya.benikov@gmail.com>
2023-04-13 22:42:23 +0400

    Created search result controller

---
+++ AboutFilm/Controllers/Films/SearchResultController.swift
@@ -0,0 +1,71 @@
+import UIKit
+
+class SearchResultController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var loader: UIView? = nil
+    var navbarTitle = "Search: "
+    var films: [SearchFilmResult?] = []{
+        didSet{
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.loader?.removeFromSuperview()
+            }
+        }
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var titleNavBar: UINavigationItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loader = Loader().getLoader(x: 0, y: 95, width: tableView.bounds.width, height: tableView.bounds.height)
+        view.addSubview(loader!)
+        
+        titleNavBar.title = navbarTitle
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "SearchResultCell", bundle: nil), forCellReuseIdentifier: "SearchResultCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        films.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchResultCell", for: indexPath) as! SearchResultCell
+        if films.isEmpty{
+            return cell
+        }
+        
+        guard let film =  films[indexPath.row] else {
+            return cell
+        }
+        
+        cell.film = film
+        cell.configureCell()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let film = films[indexPath.row] else {
+            tableView.deselectRow(at: indexPath, animated: true)
+            return
+        }
+        let destination = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "FilmDescriptionController") as! FilmDescriptionController
+        
+        destination.film = Docs(id: nil, name: film.name!, shortDescription: film.shortDescription!, description: film.description!, genres: [], rating: nil, type: nil, year: film.year, movieLength: film.movieLength, alternativeName: nil, countries: nil)
+        destination.needToGetData = false
+        destination.navbarIsHiden = false
+        destination.updateButtonIsHiden = true
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        self.navigationController?.pushViewController(destination, animated: true)
+    }
+    
+    
+    @IBAction func backButtonAction(_ sender: UIBarButtonItem) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
