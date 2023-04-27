5710304579370ce17aa56af76ed22cd34bb158d4
Ilya Benikov <Ilya.benikov@gmail.com>
2023-04-27 12:38:22 +0400

    Rewrite film description screen

--- AboutFilm/Controllers/Films/FilmDescriptionController.swift
+++ AboutFilm/Controllers/Films/FilmDescriptionController.swift
@@ -13,8 +13,6 @@ class FilmDescriptionController: UIViewController {
     private lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.alwaysBounceVertical = true
-        scrollView.frame = view.bounds
         scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height + 400)
         return scrollView
     }()
@@ -50,30 +48,53 @@ class FilmDescriptionController: UIViewController {
         return label
     }()
     
-    private let controllStackView: UIStackView = {
-        let stack = UIStackView()
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.axis = .horizontal
-        stack.spacing = 10
-        stack.alignment = .fill
-        stack.layer.borderWidth = 1
-        return stack
-    }()
+    //MARK: - Button for stack
     
+    private let buttonLike: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "Star"), for: .normal)
+        button.tintColor = .systemGray2
+        return button
+    }()
+    private let buttonBookMark: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "Favourite"), for: .normal)
+        button.tintColor = .systemGray2
+        return button
+    }()
+    private let buttonShare: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "Share"), for: .normal)
+        button.tintColor = .systemGray2
+        return button
+    }()
     
     private let descriptionTextView: UITextView = {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.textContainer.maximumNumberOfLines = 7
+        textView.isSelectable = false
+        textView.isEditable = false
         textView.textAlignment = .left
-        textView.font = .systemFont(ofSize: 14)
+        textView.font = .systemFont(ofSize: 16)
         textView.text = "Home is where we feel comfortable, safe, and loved. It's a place where we create memories with our family and friends. Home can be a physical structure or a feeling. It's where we relax, recharge, and find solace from the outside world. We decorate it with our personal items, fill it with the scents of home-cooked meals, and make it our own. Home is not just a place; it's a sense of belonging, a sanctuary from the chaos of life, and a reflection of who we are."
         return textView
+    }()
+    
+    private lazy var ratingScrollView: UIScrollView = {
+        let rating = UIScrollView()
+        rating.translatesAutoresizingMaskIntoConstraints = false
+        rating.contentSize = CGSize(width: view.frame.width, height: view.frame.height)
+        return rating
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSrollLayout()
+        setupRatingScrollView()
     }
     
     private func setupSrollLayout() {
@@ -84,28 +105,36 @@ class FilmDescriptionController: UIViewController {
         title.translatesAutoresizingMaskIntoConstraints = false
         title.text = "FilmTitle"
         
-//        navigationBar.addSubview(title)
+        let controllStackView = UIStackView(arrangedSubviews: [buttonLike, buttonBookMark, buttonShare])
+        controllStackView.translatesAutoresizingMaskIntoConstraints = false
+        controllStackView.axis = .horizontal
+        controllStackView.distribution = .fillEqually
+        
+        navigationBar.addSubview(title)
         
         view.addSubview(scrollView)
-//        view.addSubview(navigationBar)
+        view.addSubview(navigationBar)
         
         scrollView.addSubview(imageViewContainer)
         scrollView.addSubview(filmsParamLabel)
         scrollView.addSubview(actorsLabel)
         scrollView.addSubview(descriptionTextView)
         scrollView.addSubview(controllStackView)
+        scrollView.addSubview(ratingScrollView)
+        
         scrollView.addSubview(imageViewContainer)
+    
         
         imageViewContainer.addSubview(imageView)
         
         NSLayoutConstraint.activate([
-//            navigationBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-//            navigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            navigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-//            navigationBar.heightAnchor.constraint(equalToConstant: 50),
+            navigationBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            navigationBar.heightAnchor.constraint(equalToConstant: 50),
             
-//            title.centerXAnchor.constraint(equalTo: navigationBar.centerXAnchor),
-//            title.centerYAnchor.constraint(equalTo: navigationBar.centerYAnchor),
+            title.centerXAnchor.constraint(equalTo: navigationBar.centerXAnchor),
+            title.centerYAnchor.constraint(equalTo: navigationBar.centerYAnchor),
 //
             
             scrollView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -135,15 +164,117 @@ class FilmDescriptionController: UIViewController {
             actorsLabel.heightAnchor.constraint(equalToConstant: 50),
             
             controllStackView.topAnchor.constraint(equalTo: actorsLabel.bottomAnchor, constant: 10),
-            controllStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            controllStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            controllStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            controllStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             controllStackView.heightAnchor.constraint(equalToConstant: 60),
             
             descriptionTextView.topAnchor.constraint(equalTo: controllStackView.bottomAnchor, constant: 10),
             descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            descriptionTextView.heightAnchor.constraint(equalToConstant: 150)
+            descriptionTextView.heightAnchor.constraint(equalToConstant: 150),
+            
+            ratingScrollView.topAnchor.constraint(equalTo: descriptionTextView.bottomAnchor, constant: 15),
+            ratingScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            ratingScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            ratingScrollView.heightAnchor.constraint(equalToConstant: 100)
         ])
     }
+    
+
+//MARK: - Setup Scroll Rating
+    
+    private func setupRatingScrollView() {
+        
+        let vote: UIView = {
+            let vote = UIView()
+            let numLabel = UILabel()
+            let descLabel = UILabel()
+            
+            vote.addSubview(numLabel)
+            vote.addSubview(descLabel)
+            
+            vote.translatesAutoresizingMaskIntoConstraints = false
+            numLabel.translatesAutoresizingMaskIntoConstraints = false
+            descLabel.translatesAutoresizingMaskIntoConstraints = false
+            
+            vote.backgroundColor = .mainGray
+
+            
+            numLabel.text = "9.0"
+            numLabel.font = .boldSystemFont(ofSize: 30)
+            
+            descLabel.text = "Rating IMDb"
+            descLabel.font = .systemFont(ofSize: 16)
+            descLabel.numberOfLines = 2
+            
+            NSLayoutConstraint.activate([
+                numLabel.topAnchor.constraint(equalTo: vote.topAnchor),
+                numLabel.leadingAnchor.constraint(equalTo: vote.leadingAnchor, constant: 20),
+                numLabel.bottomAnchor.constraint(equalTo: vote.bottomAnchor),
+                numLabel.widthAnchor.constraint(equalToConstant: 70),
+                
+                descLabel.topAnchor.constraint(equalTo: vote.topAnchor),
+                descLabel.leadingAnchor.constraint(equalTo: numLabel.trailingAnchor),
+                descLabel.trailingAnchor.constraint(equalTo: vote.trailingAnchor),
+                descLabel.bottomAnchor.constraint(equalTo: vote.bottomAnchor)
+            ])
+            return vote
+        }()
+        
+        let vote2: UIView = {
+            let vote = UIView()
+            let numLabel = UILabel()
+            let descLabel = UILabel()
+            
+            vote.addSubview(numLabel)
+            vote.addSubview(descLabel)
+            
+            vote.translatesAutoresizingMaskIntoConstraints = false
+            numLabel.translatesAutoresizingMaskIntoConstraints = false
+            descLabel.translatesAutoresizingMaskIntoConstraints = false
+            
+            vote.backgroundColor = .mainGray
+
+            
+            numLabel.text = "9.0"
+            numLabel.font = .boldSystemFont(ofSize: 30)
+            
+            descLabel.text = "Rating IMDb"
+            descLabel.font = .systemFont(ofSize: 16)
+            descLabel.numberOfLines = 2
+            
+            NSLayoutConstraint.activate([
+                numLabel.topAnchor.constraint(equalTo: vote.topAnchor),
+                numLabel.leadingAnchor.constraint(equalTo: vote.leadingAnchor, constant: 20),
+                numLabel.bottomAnchor.constraint(equalTo: vote.bottomAnchor),
+                numLabel.widthAnchor.constraint(equalToConstant: 70),
+                
+                descLabel.topAnchor.constraint(equalTo: vote.topAnchor),
+                descLabel.leadingAnchor.constraint(equalTo: numLabel.trailingAnchor),
+                descLabel.trailingAnchor.constraint(equalTo: vote.trailingAnchor),
+                descLabel.bottomAnchor.constraint(equalTo: vote.bottomAnchor)
+            ])
+            return vote
+        }()
+        
+        ratingScrollView.addSubview(vote)
+        ratingScrollView.addSubview(vote2)
+        
+        NSLayoutConstraint.activate([
+            vote.topAnchor.constraint(equalTo: ratingScrollView.topAnchor),
+            vote.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            vote.bottomAnchor.constraint(equalTo: ratingScrollView.bottomAnchor),
+            vote.widthAnchor.constraint(equalToConstant: 200),
+            vote.heightAnchor.constraint(equalToConstant: 100),
+            
+            vote2.topAnchor.constraint(equalTo: ratingScrollView.topAnchor),
+            vote2.leadingAnchor.constraint(equalTo: vote.trailingAnchor, constant: 15),
+            vote2.bottomAnchor.constraint(equalTo: ratingScrollView.bottomAnchor),
+            vote2.heightAnchor.constraint(equalToConstant: 100),
+            vote2.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    
     
 }
