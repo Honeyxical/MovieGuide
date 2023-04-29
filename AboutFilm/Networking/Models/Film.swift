774c8faf4d8f6d917a52ffb6c7d19ba5fed57ada
Ilya Benikov <Ilya.benikov@gmail.com>
2023-04-29 19:16:35 +0400

    Added variable for image data Deleted unused struct

--- AboutFilm/Networking/Models/Film.swift
+++ AboutFilm/Networking/Models/Film.swift
@@ -19,6 +19,7 @@ struct FilmFullInfo: Decodable{
     let rating: Rating?
     
     var poster: Poster?
+    var posterData: Data?
     
     let genres: [Generes]?
     
@@ -28,7 +29,6 @@ struct FilmFullInfo: Decodable{
     
     let similarMovies: [SimilarMovies]?
     
-    let logo: Logo?
 }
 
 struct Rating: Decodable{
@@ -37,20 +37,6 @@ struct Rating: Decodable{
     let filmCritics: Double?
     let russianFilmCritics: Double?
     let await: Double?
-}
-
-struct ExternalId: Decodable {
-    let kpHD: String?
-    let imdb: String?
-    let tmdb: Int?
-}
-
-struct Votes: Decodable{
-    let kinopoisk: Int?
-    let imdb: Int?
-    let filmCritics: Int?
-    let russianFilmCritics: Int?
-    let await: Int?
 }
 
 struct Premiere: Decodable{
@@ -70,10 +56,6 @@ struct Backdrop: Decodable{
     let previewUrl: String?
 }
 
-struct Logo: Decodable {
-    let url: String?
-}
-
 struct Person: Decodable{
     let id: Int?
     let photo: String?
@@ -81,6 +63,7 @@ struct Person: Decodable{
     let enName: String?
     let profession: String?
     let enProfession: String?
+    var photoData: Data?
 }
 
 struct SimilarMovies: Decodable{
@@ -90,21 +73,8 @@ struct SimilarMovies: Decodable{
     let alternativeName: String?
     let type: String?
     let poster: Poster?
+    var posterData: Data?
 }
-
-struct Videos: Decodable {
-    let trailers: [Trailer]?
-    let teasers: [Trailer]?
-}
-
-struct Trailer: Decodable{
-    let url: String?
-    let name: String?
-    let site: String?
-    let type: String?
-}
-
-// Переделать в енам :
 
 struct Generes: Decodable{
     let name: String?
@@ -113,6 +83,5 @@ struct Countries: Decodable{
     let name: String?
 }
 
-// Seasons info dobavit
 
 
