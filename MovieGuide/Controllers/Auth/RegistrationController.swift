337e983eb498cf405f44f8edc510f0c03f69f652
Ilya Benikov <ilya.benikov@gmail.com>
2023-06-13 01:34:43 +0400

    Rewrite registration screen

--- MovieGuide/Controllers/Auth/RegistrationController.swift
+++ MovieGuide/Controllers/Auth/RegistrationController.swift
@@ -1,46 +1,44 @@
 import UIKit
 
 class RegistrationController: UIViewController {
-    private let nicknameTF: UITextField = {
-        let tf = UITextField()
-        tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Nickname"
-        tf.borderStyle = .roundedRect
-        tf.autocorrectionType = .no
-        return tf
+    
+    private let backButton: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        btn.tintColor = .black
+        btn.addTarget(nil, action: #selector(back), for: .touchUpInside)
+        return btn
     }()
     
-    private let email: UITextField = {
-        let tf = UITextField()
-        tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Email"
-        tf.borderStyle = .roundedRect
-        tf.autocorrectionType = .no
-        return tf
+    @objc private func back(){
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private let registerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.attributedText = NSAttributedString(
+            string: "Personal details",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 36, weight: .bold)])
+        return label
     }()
     
-    private let loginTF: UITextField = {
-        let tf = UITextField()
-        tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Login"
-        tf.borderStyle = .roundedRect
-        tf.autocorrectionType = .no
-        return tf
-    }()
+    private let nicknameTF: UITextField = getTextField(plaseholder: "Nickname")
     
-    private let passwordTF: UITextField = {
-        let tf = UITextField()
-        tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Password"
-        tf.borderStyle = .roundedRect
-        tf.isSecureTextEntry = false
-        return tf
-    }()
+    private let email: UITextField = getTextField(plaseholder: "Email")
+    
+    private let loginTF: UITextField = getTextField(plaseholder: "Login")
+    
+    private let passwordTF: UITextField = getTextField(plaseholder: "Password")
     
     private let registerButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.setTitle("Registration", for: .normal)
+        btn.setAttributedTitle(NSAttributedString(
+            string: "SIGN UP",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont(name: "Arial", size: 18)!]), for: .normal)
+        btn.backgroundColor = .black
         btn.addTarget(nil, action: #selector(registrationButton), for: .touchUpInside)
         return btn
     }()
@@ -58,6 +56,25 @@ class RegistrationController: UIViewController {
         }
     }
     
+    private let haveACLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.attributedText = NSAttributedString(
+            string: "Already have an account?",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.font: UIFont(name: "Arial", size: 18)!])
+        return label
+    }()
+    
+    private let signInButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setAttributedTitle(NSAttributedString(
+            string: "Sign In",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18, weight: .bold)]), for: .normal)
+        btn.addTarget(nil, action: #selector(back), for: .touchUpInside)
+        return btn
+    }()
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
@@ -69,33 +86,55 @@ class RegistrationController: UIViewController {
     }
     
     private func setupLayout() {
+        view.addSubview(backButton)
+        view.addSubview(registerLabel)
         view.addSubview(nicknameTF)
         view.addSubview(loginTF)
         view.addSubview(email)
         view.addSubview(passwordTF)
         view.addSubview(registerButton)
+        view.addSubview(haveACLabel)
+        view.addSubview(signInButton)
         
         NSLayoutConstraint.activate([
+            backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 75),
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            backButton.heightAnchor.constraint(equalToConstant: 25),
+            backButton.widthAnchor.constraint(equalToConstant: 25),
+            
+            registerLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 125),
+            registerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            
             nicknameTF.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            nicknameTF.topAnchor.constraint(equalTo: view.topAnchor, constant: 375),
+            nicknameTF.topAnchor.constraint(equalTo: view.topAnchor, constant: 225),
             nicknameTF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             nicknameTF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             
-            email.topAnchor.constraint(equalTo: nicknameTF.bottomAnchor, constant: 10),
+            email.topAnchor.constraint(equalTo: nicknameTF.bottomAnchor, constant: 60),
             email.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             email.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             
-            loginTF.topAnchor.constraint(equalTo: email.bottomAnchor, constant: 10),
+            loginTF.topAnchor.constraint(equalTo: email.bottomAnchor, constant: 60),
             loginTF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             loginTF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             
-            passwordTF.topAnchor.constraint(equalTo: loginTF.bottomAnchor, constant: 10),
+            passwordTF.topAnchor.constraint(equalTo: loginTF.bottomAnchor, constant: 60),
             passwordTF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             passwordTF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             
             registerButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -200),
             registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            registerButton.widthAnchor.constraint(equalToConstant: 200)
+            registerButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            registerButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            registerButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            haveACLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            haveACLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -33),
+            haveACLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            signInButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            signInButton.leadingAnchor.constraint(equalTo: haveACLabel.trailingAnchor, constant: 2),
+            signInButton.heightAnchor.constraint(equalToConstant: 20)
         ])
     }
     
