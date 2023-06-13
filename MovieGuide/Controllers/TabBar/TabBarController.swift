cac061bf5c7d6efa50bac167810a08beb971fd04
Ilya Benikov <ilya.benikov@gmail.com>
2023-06-13 22:19:53 +0400

    Rewrited TabBarController

--- MovieGuide/Controllers/TabBar/TabBarController.swift
+++ MovieGuide/Controllers/TabBar/TabBarController.swift
@@ -1,17 +1,31 @@
 import UIKit
 
+enum tabBarUnselected: String {
+    case home = "house.circle"
+    case film = "film.circle"
+    case search = "magnifyingglass.circle"
+    case profile = "person.circle"
+}
+
 class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupVCs()
+        
+        tabBar.backgroundImage = UIImage()
+        tabBar.shadowImage = UIImage()
+        tabBar.tintColor = .white
+        
+        setupTabBarLayout()
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
     }
     
     func setupVCs() {
             viewControllers = [
-                createNavController(for: HomeController(), title: "Home", image: UIImage(systemName: "house")!),
-                createNavController(for: FilmController(), title: "Random film", image: UIImage(systemName: "film.circle")!),
-                createNavController(for: SearchController(), title: "Search", image: UIImage(systemName: "magnifyingglass")!),
-                createNavController(for: ProfileController(), title: "Profile", image: UIImage(systemName: "person")!),
+                createNavController(for: HomeController(), title: "Home", image: UIImage(systemName: tabBarUnselected.home.rawValue)!),
+                createNavController(for: FilmController(), title: "Random film", image: UIImage(systemName: tabBarUnselected.film.rawValue)!),
+                createNavController(for: SearchController(), title: "Search", image: UIImage(systemName: tabBarUnselected.search.rawValue)!),
+                createNavController(for: ProfileController(), title: "Profile", image: UIImage(systemName: tabBarUnselected.profile.rawValue)!),
             ]
         }
     
@@ -24,5 +38,24 @@ class TabBarController: UITabBarController {
         navController.navigationBar.isHidden = true
         navigationController?.navigationBar.isHidden = true
         return navController
+    }
+    
+    fileprivate func setupTabBarLayout() {
+        let island = UIView()
+        
+        island.translatesAutoresizingMaskIntoConstraints = false
+        island.backgroundColor = .black
+        island.layer.cornerRadius = 25
+        
+        island.addSubview(tabBar)
+        view.addSubview(island)
+        
+        NSLayoutConstraint.activate([
+            island.heightAnchor.constraint(equalToConstant: 65),
+            island.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            island.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            island.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            island.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -25)
+        ])
     }
 }
